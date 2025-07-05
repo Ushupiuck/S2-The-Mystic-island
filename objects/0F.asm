9bf05143b3d155742f536916e857ff7dd99bd387
Ushupiuck <giancarloknd@hotmail.com>
2025-07-05 01:03:13 -0300

    Update 0F.asm

--- objects/0F.asm
+++ objects/0F.asm
@@ -1,5 +1,5 @@
 ; ---------------------------------------------------------------------------
-; Object 0F - Mappings test?
+; Object 0F - Blank
 ; ---------------------------------------------------------------------------
 
 Obj0F:
@@ -10,29 +10,12 @@ Obj0F:
 		bra.w	DisplaySprite
 ; ===========================================================================
 Obj0F_Index:	dc.w Obj0F_Init-Obj0F_Index
-		dc.w Obj0F_Cycle-Obj0F_Index
-		dc.w Obj0F_Cycle-Obj0F_Index
+		dc.w Obj0F_Delete-Obj0F_Index
 ; ===========================================================================
 
 Obj0F_Init:
 		addq.b	#2,obRoutine(a0)
-		move.w	#$90,obX(a0)
-		move.w	#$90,obScreenX(a0)
-		move.l	#Map_Obj0F,obMap(a0)
-		move.w	#make_art_tile(ArtTile_Monitor,0,0),obGfx(a0)
-		bsr.w	Adjust2PArtPointer
-
-Obj0F_Cycle:
-		move.b	(v_jpadpress1).w,d0
-		btst	#bitC,d0			; has C been pressed?
-		beq.s	Obj0F_Toggle			; if not, branch
-		addq.b	#1,obFrame(a0)			; increment mappings
-		andi.b	#$F,obFrame(a0)			; if above $F, reset
-
-Obj0F_Toggle:
-		btst	#bitB,d0			; has B been pressed?
-		beq.s	.donothing			; if not, branch
-		bchg	#0,(unk_FFE9).w			; try turning on two player mode which will not work...
-
-.donothing:
 		rts
+
+Obj0F_Delete:
+		bra.w	DeleteObject
