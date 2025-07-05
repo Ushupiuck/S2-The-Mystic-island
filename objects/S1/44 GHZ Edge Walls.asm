c782d99f7bfdb1a7502daa540a898bc07b252640
Ushupiuck <giancarloknd@hotmail.com>
2025-07-05 06:37:39 -0300

    Update 44 GHZ Edge Walls.asm

--- objects/S1/44 GHZ Edge Walls.asm
+++ objects/S1/44 GHZ Edge Walls.asm
@@ -10,22 +10,20 @@ Obj44:
 ; ===========================================================================
 Edge_Index:	dc.w Edge_Main-Edge_Index
 		dc.w Edge_Solid-Edge_Index
-		dc.w Edge_Display-Edge_Index
 ; ===========================================================================
 
 Edge_Main:	; Routine 0
 		addq.b	#2,obRoutine(a0)
 		move.l	#Map_obj44,obMap(a0)
 		move.w	#make_art_tile(ArtTile_GHZ_Edge_Wall,2,0),obGfx(a0)
-		bsr.w	Adjust2PArtPointer
 		ori.b	#4,obRender(a0)
 		move.b	#8,obActWid(a0)
 		move.b	#6,obPriority(a0)
 		move.b	obSubtype(a0),obFrame(a0) ; copy object type number to frame number
 		bclr	#4,obFrame(a0)	; clear	4th bit	(deduct	$10)
 		beq.s	Edge_Solid	; make object solid if 4th bit = 0
-		addq.b	#2,obRoutine(a0)
-		bra.s	Edge_Display	; don't make it solid if 4th bit = 1
+		out_of_range.w	DeleteObject
+		bra.w	DisplaySprite	; don't make it solid if 4th bit = 1
 ; ===========================================================================
 
 Edge_Solid:	; Routine 2
@@ -35,7 +33,5 @@ Edge_Solid:	; Routine 2
 		addq.w	#1,d3
 		move.w	obX(a0),d4
 		bsr.w	SolidObject
-
-Edge_Display:	; Routine 4
 		out_of_range.w	DeleteObject
 		bra.w	DisplaySprite
