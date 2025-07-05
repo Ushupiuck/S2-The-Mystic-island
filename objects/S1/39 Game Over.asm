afed2a66d2bd4a1d65c93bfea880ba6d03e4592d
Ushupiuck <giancarloknd@hotmail.com>
2025-07-05 06:23:47 -0300

    Update 39 Game Over.asm

--- objects/S1/39 Game Over.asm
+++ objects/S1/39 Game Over.asm
@@ -30,7 +30,6 @@ Over_1stWord:
 		move.w	#$F0,obScreenX(a0)
 		move.l	#Map_Over,obMap(a0)
 		move.w	#make_art_tile(ArtTile_Game_Over,0,1),obGfx(a0)
-		bsr.w	Adjust2PArtPointer
 		move.b	#0,obRender(a0)
 		move.b	#0,obPriority(a0)
 
