7d528943aa23d341df2092f4e0a1e48338701129
Ushupiuck <giancarloknd@hotmail.com>
2025-07-05 06:39:51 -0300

    Update 4A Special Stage Entry (Unused).asm

--- objects/S1/4A Special Stage Entry (Unused).asm
+++ objects/S1/4A Special Stage Entry (Unused).asm
@@ -29,7 +29,6 @@ loc_124D4:
 		move.b	#1,obPriority(a0)
 		move.b	#$38,obActWid(a0)
 		move.w	#make_art_tile(ArtTile_Warp,0,0),obGfx(a0)
-		bsr.w	Adjust2PArtPointer
 		move.w	#60*2,obj4A_vanishtime(a0)	; set vanishing time to 2 seconds
 
 S1Obj4A_RmvSonic:
