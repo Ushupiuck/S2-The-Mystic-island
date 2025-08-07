239056c774d934dc9113cfec2fe26ce67fa3687c
Ushupiuck <giancarloknd@hotmail.com>
2025-08-07 04:56:19 -0300

    Update constants.asm

--- constants.asm
+++ constants.asm
@@ -4,77 +4,77 @@
 ; ---------------------------------------------------------------------------
 
 ; Object variables
-obID:		equ 0					; object ID number
-obRender:	equ 1					; bitfield for x/y flip, display mode
-obGfx:		equ 2					; palette line & VRAM setting (2 bytes)
-obMap:		equ 4					; mappings address (4 bytes)
-obX:		equ 8					; x-axis position (2-4 bytes)
-obXSub:		equ $A					; for when exra presition is required (2 bytes)
-obY:		equ $C					; y-axis position (2-4 bytes)
-obYSub:		equ $E					; for when exra presition is required (2 bytes)
-obVelX:		equ $10					; x-axis velocity (2 bytes)
-obVelY:		equ $12					; y-axis velocity (2 bytes)
-obInertia:	equ $20					; potential speed (2 bytes)
-obHeight:	equ $16					; height/2; y_radius
-obWidth:	equ $14					; width/2 ; x_radius
-obPriority:	equ $18					; sprite stack priority -- 0 is front
-obActWid:	equ $19					; action width
-obFrame:	equ $1A					; current frame displayed
-obAniFrame:	equ $1B					; current frame in animation script
-obAnim:		equ $1C					; current animation
-obPrevAni:	equ $1D					; previous animation
-obTimeFrame:	equ $1E					; time to next frame
-obDelayAni:	equ $1F					; time to delay animation
-obColType:	equ $20					; collision response type
-obColProp:	equ $21					; collision extra property
-obStatus:	equ $22					; orientation or mode
-obRespawnNo:	equ $23					; respawn list index number
-obRoutine:	equ $24					; routine number
-ob2ndRout:	equ $25					; secondary routine number
-obAngle:	equ $26					; angle
-obSubtype:	equ $28					; object subtype
-obControl:	equ $2A					; 0 for normal, 1 for hanging or for resting on a flipper, $81 for going through CNZ/OOZ/MTZ tubes or stopped in CNZ cages or stoppers or flying if Tails
-obSolid:	equ ob2ndRout				; solid status flag
+obID:			equ 0		; object ID number
+obRender:		equ 1		; bitfield for x/y flip, display mode
+obGfx:			equ 2		; palette line & VRAM setting (2 bytes)
+obMap:			equ 4		; mappings address (4 bytes)
+obX:			equ 8		; x-axis position (2-4 bytes)
+obXSub:			equ $A		; for when exra presition is required (2 bytes)
+obY:			equ $C		; y-axis position (2-4 bytes)
+obYSub:			equ $E		; for when exra presition is required (2 bytes)
+obVelX:			equ $10		; x-axis velocity (2 bytes)
+obVelY:			equ $12		; y-axis velocity (2 bytes)
+obInertia:		equ $20		; potential speed (2 bytes)
+obHeight:		equ $16		; height/2; y_radius
+obWidth:		equ $14		; width/2 ; x_radius
+obPriority:		equ $18		; sprite stack priority -- 0 is front
+obActWid:		equ $19		; action width
+obFrame:		equ $1A		; current frame displayed
+obAniFrame:		equ $1B		; current frame in animation script
+obAnim:			equ $1C		; current animation
+obPrevAni:		equ $1D		; previous animation
+obTimeFrame:		equ $1E		; time to next frame
+obDelayAni:		equ $1F		; time to delay animation
+obColType:		equ $20		; collision response type
+obColProp:		equ $21		; collision extra property
+obStatus:		equ $22		; orientation or mode
+obRespawnNo:		equ $23		; respawn list index number
+obRoutine:		equ $24		; routine number
+ob2ndRout:		equ $25		; secondary routine number
+obAngle:		equ $26		; angle
+obSubtype:		equ $28		; object subtype
+obControl:		equ $2A		; 0 for normal, 1 for hanging or for resting on a flipper, $81 for going through CNZ/OOZ/MTZ tubes or stopped in CNZ cages or stoppers or flying if Tails
+obSolid:	equ ob2ndRout		; solid status flag
 
-obTopSolidBit:	equ $3E					; bit to check for top solidity (either $C or $E)
-obLRBSolidBit:	equ $3F					; bit to check for left/right/bottom solidity (either $D or $F)
+obTopSolidBit:		equ $3E		; bit to check for top solidity (either $C or $E)
+obLRBSolidBit:		equ $3F		; bit to check for left/right/bottom solidity (either $D or $F)
 
 ; Object variables used by Sonic/Tails
-; v_air:	equ $28
-flashtime:	equ $30					; time between flashes after getting hit
-invtime:	equ $32					; time left for invincibility
-shoetime:	equ $34					; time left for speed shoes
-stick_to_convex:	equ objoff_38
-spindash_flag:	equ $39					; 0 for normal, 1 for charging a spindash or forced rolling
-standonobject:	equ $3D					; object Sonic stands on
+; v_air:		equ $28
+flashtime:		equ $30		; time between flashes after getting hit
+invtime:		equ $32		; time left for invincibility
+shoetime:		equ $34		; time left for speed shoes
+stick_to_convex:	equ $38
+spindash_flag:		equ $39		; 0 for normal, 1 for charging a spindash or forced rolling
+standonobject:		equ $3D		; object Sonic stands on
 
 ; ---------------------------------------------------------------------------
 ; Miscellaneous object scratch-RAM
-objoff_25:	equ $25
-objoff_26:	equ $26
-objoff_27:	equ $27
-objoff_29:	equ $29
-objoff_2A:	equ $2A
-objoff_2B:	equ $2B
-objoff_2C:	equ $2C
-objoff_2D:	equ $2D
-objoff_2E:	equ $2E
-objoff_2F:	equ $2F
-objoff_30:	equ $30
-objoff_32:	equ $32
-objoff_33:	equ $33
-objoff_34:	equ $34
-objoff_35:	equ $35
-objoff_36:	equ $36
-objoff_37:	equ $37
-objoff_38:	equ $38
-objoff_39:	equ $39
-objoff_3A:	equ $3A
-objoff_3B:	equ $3B
-objoff_3C:	equ $3C
-objoff_3D:	equ $3D
-objoff_3E:	equ $3E
-objoff_3F:	equ $3F
+objoff_25:		equ $25
+objoff_26:		equ $26
+objoff_27:		equ $27
+objoff_29:		equ $29
+objoff_2A:		equ $2A
+objoff_2B:		equ $2B
+objoff_2C:		equ $2C
+objoff_2D:		equ $2D
+objoff_2E:		equ $2E
+objoff_2F:		equ $2F
+objoff_30:		equ $30
+objoff_32:		equ $32
+objoff_33:		equ $33
+objoff_34:		equ $34
+objoff_35:		equ $35
+objoff_36:		equ $36
+objoff_37:		equ $37
+objoff_38:		equ $38
+objoff_39:		equ $39
+objoff_3A:		equ $3A
+objoff_3B:		equ $3B
+objoff_3C:		equ $3C
+objoff_3D:		equ $3D
+objoff_3E:		equ $3E
+objoff_3F:		equ $3F
 ; ---------------------------------------------------------------------------
 ; conventions followed by several objects but NOT Sonic/Tails:
 obScreenX =		obX ; and 1+x_pos ; x coordinate for objects using screen-space coordinate system (S2 x_pixel)
@@ -82,7 +82,7 @@ obScreenY =		obXSub ; and 3+x_pos ; y coordinate for objects using screen-space 
 parent =		objoff_3E ; and $3F ; address of object that owns or spawned this one, if applicable
 
 object_size_bits:	equ 6
-object_size:	equ 1<<object_size_bits
+object_size:		equ 1<<object_size_bits
 next_object =		object_size
 
 ; ---------------------------------------------------------------------------
