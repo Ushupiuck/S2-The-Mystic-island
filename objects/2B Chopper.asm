99da0cfd5127232fa02c9654fdb8c4bdbe6037e5
Ushupiuck <giancarloknd@hotmail.com>
2025-08-08 03:15:04 -0300

    Revert "Chopper rework"
    This reverts commit e7a1f472c7658e2cee369f87b50f7a93e0031c78.

--- objects/2B Chopper.asm
+++ objects/2B Chopper.asm
@@ -11,18 +11,11 @@ Obj2B:
 ; ===========================================================================
 Chop_Index:	dc.w Chop_Main-Chop_Index
 		dc.w Chop_ChgSpeed-Chop_Index
-chop_origY = obYSub	; word - ground level
-chop_velY = obVelX	; long - fixed vertical speed
-chop_grav = objoff_2A	; long - self explanatory
-chop_posY = objoff_2E	; long - current y position
+chop_origY = objoff_30
 ; ===========================================================================
 
 Chop_Main:	; Routine 0
 		addq.b	#2,obRoutine(a0)
-		move.b	#4,obRender(a0)
-		move.b	#4,obPriority(a0)
-		move.b	#9,obColType(a0)
-		move.b	#$10,obActWid(a0)
 		move.l	#Map_Obj2B,obMap(a0)
 		move.w	#make_art_tile(ArtTile_Chopper,0,0),obGfx(a0)
 		tst.b	(Current_Zone).w
@@ -30,65 +23,43 @@ Chop_Main:	; Routine 0
 		move.l	#Map_obj2B_1,obMap(a0)
 		move.w	#make_art_tile(ArtTile_Masher,0,0),obGfx(a0)
 .notEHZ:
-		move.w	obY(a0),d0
-		move.w	d0,chop_origY(a0)     ; Save ground level
-		lsl.l	#8,d0                 ; Convert to fixed point
-		move.l	d0,chop_posY(a0)      ; Set true position
-
+		move.b	#4,obRender(a0)
+		move.b	#4,obPriority(a0)
+		move.b	#9,obColType(a0)
+		move.b	#$10,obActWid(a0)
 		move.b	obSubtype(a0),d1	; get subtype (for vertical speed)
 		lsl.w	#1,d1			; filter out which entry it is
-		move.w	Chopper_JumpHeights(pc,d1.w),d0		; and store the vertical speed
-		ext.l	d0
-		move.l	d0,chop_velY(a0)
-		move.l	#$0018,chop_grav(a0)
-		rts
+		move.w	Chopper_JumpHeights(pc,d1.w),obVelY(a0)		; and store the vertical speed
+		move.w	obY(a0),chop_origY(a0)	; save original position
 
+Chop_ChgSpeed:	; Routine 2
+		lea	(Ani_Obj2B).l,a1
+		bsr.w	AnimateSprite
+		bsr.w	ObjectMove
+		addi.w	#$18,obVelY(a0)	; reduce speed
+		move.w	chop_origY(a0),d0
+		cmp.w	obY(a0),d0	; has Chopper returned to its original position?
+		bcc.s	.chganimation	; if not, branch
+		move.w	d0,obY(a0)
+		move.b	obSubtype(a0),d1	; get subtype (for vertical speed)
+		lsl.w	#1,d1			; filter out which entry it is
+		move.w	Chopper_JumpHeights(pc,d1.w),obVelY(a0)		; and store the vertical speed
+
+.chganimation:
+		move.b	#1,obAnim(a0)	; use fast animation
+		subi.w	#$C0,d0
+		cmp.w	obY(a0),d0
+		bcc.s	.nochg
+		move.b	#0,obAnim(a0)	; use slow animation
+		tst.w	obVelY(a0)	; is Chopper at	its highest point?
+		bmi.s	.nochg		; if not, branch
+		move.b	#2,obAnim(a0)	; use stationary animation
+
+.nochg:
+		rts
 ; ===========================================================================
 Chopper_JumpHeights:
 		dc.w	-$400, -$480	; 1, 2
 		dc.w	-$500, -$580	; 3, 4
 		dc.w	-$600, -$680	; 5, 6
 		dc.w	-$700, -$780	; 7, 8
-; ===========================================================================
-
-Chop_ChgSpeed:	; Routine 2
-		lea	(Ani_Obj2B).l,a1
-		bsr.w	AnimateSprite
-		; Update subpixel position
-		move.l	chop_velY(a0),d0
-		add.l	d0,chop_posY(a0)
-		; Convert to visible pixel position
-		move.l	chop_posY(a0),d0
-		lsr.l	#8,d0
-		move.w	d0,obY(a0)
-		; Gravity effect
-		move.l	chop_grav(a0),d1
-		add.l	d1,chop_velY(a0)
-		move.b	#1,obAnim(a0)	; we default to the fast animation
-		move.w	chop_origY(a0),d2
-		subi.w	#$C0,d2
-		cmp.w	obY(a0),d2
-		bcc.s	.CheckApex
-		move.b	#0,obAnim(a0)	; use slow animation
-		tst.l	chop_velY(a0)	; is Chopper at	its highest point?
-		bmi.s	.CheckApex	; if not, branch
-		move.b	#2,obAnim(a0)	; use stationary animation
-
-.CheckApex:
-		; If past ground level, reset position and jump
-		move.w	obY(a0),d0
-		cmp.w	chop_origY(a0),d0
-		ble.s	.done
-		; Clamp position
-		move.w	chop_origY(a0),obY(a0)
-		lsl.l	#8,d0
-		move.l	d0,chop_posY(a0)
-		; Reset jump velocity
-		move.b	obSubtype(a0),d1
-		lsl.w	#1,d1
-		move.w	Chopper_JumpHeights(pc,d1.w),d0
-		ext.l	d0
-		move.l	d0,chop_velY(a0)
-
-.done:
-		rts
