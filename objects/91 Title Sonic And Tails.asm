8eb4d53fd5c7f93ba1cacbf2a792564a6c55ad25
Ushupiuck <giancarloknd@hotmail.com>
2025-10-22 20:32:40 -0300

    re-arrange the SST's
    obTimeFrame has been relocated, and two SST's have been moved to their final locations.

--- objects/91 Title Sonic And Tails.asm
+++ objects/91 Title Sonic And Tails.asm
@@ -21,7 +21,7 @@ TSon_Main:	; Routine 0
 		move.l	#Map_TitleST,obMap(a0)
 		move.w	#make_art_tile(ArtTile_Title_Sonic_And_Tails,2,0),obGfx(a0)
 		move.b	#1,obPriority(a0)
-		move.b	#29,obDelayAni(a0)	; set time delay to 0.5 seconds
+	;	move.b	#29,obDelayAni(a0)	; set time delay to 0.5 seconds
 		tst.b	obFrame(a0)		; are we on frame 0?
 		beq.s	TSon_Delay		; if so, skip.
 		move.w	#$FC,obScreenX(a0)	; this part of code loads Tails on the Title Screen.
@@ -31,29 +31,29 @@ TSon_Main:	; Routine 0
 TSon_Delay:	; Routine 2
 		bra.w	DisplaySprite
 	; Dead code from Sonic 1, remove or comment out the branch above to use the original code.
-		subq.b	#1,obDelayAni(a0)	; subtract 1 from time delay
-		bpl.s	.wait			; if time remains, branch
-		addq.b	#2,obRoutine(a0)	; go to next routine
-		bra.w	DisplaySprite
+	;	subq.b	#1,obDelayAni(a0)	; subtract 1 from time delay
+	;	bpl.s	.wait			; if time remains, branch
+	;	addq.b	#2,obRoutine(a0)	; go to next routine
+	;	bra.w	DisplaySprite
 
 .wait:
-		rts
+	;	rts
 ; ===========================================================================
 ; This is also dead code because the routine is never actually reached.
 ; Even if it did, the animation is long gone.
 
 TSon_Move:	; Routine 4
-		subq.w	#8,obScreenY(a0)	; move Sonic up
-		cmpi.w	#$96,obScreenY(a0)	; has Sonic reached final position?
-		bne.s	.display		; if not, branch
-		addq.b	#2,obRoutine(a0)
+	;	subq.w	#8,obScreenY(a0)	; move Sonic up
+	;	cmpi.w	#$96,obScreenY(a0)	; has Sonic reached final position?
+	;	bne.s	.display		; if not, branch
+	;	addq.b	#2,obRoutine(a0)
 
 .display:
-		bra.w	DisplaySprite
+	;	bra.w	DisplaySprite
 ; ===========================================================================
 ; In Sonic 1, this would've been where the Title Character animations take place.
 ; It appears they just removed the code for animating the object.
 TSon_Animate:	; Routine 6
 	;	lea	(Ani_TSon).l,a1
 	;	bsr.w	AnimateSprite
-		bra.w	DisplaySprite
+	;	bra.w	DisplaySprite
